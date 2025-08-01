82bbc410efdc7522d5d1778db61959abe0391c32
gabythaila <gtma@aluno.ifnmg.edu.br>
2025-08-01 12:01:17 +0000

    .

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-// Criando um novo elemento 
-let novoElemento = document.createElement('h1');
-// Alterando o conteúdo de texto do elemento
-novoElemento.innerText = 'Hello, World! English! (Inglês) Ok?!';
-// Selecionando o elemento body
-let elementoBody = document.body;
-// Colocando o novo elemento no body
-elementoBody.appendChild(novoElemento);
-novoElemento.style.backgroundColor = 'blue';
-novoElemento.style.color = 'yellow';
 document.addEventListener("DOMContentLoaded", function() {
     // Criação da estrutura básica do jogo
     const body = document.body;
